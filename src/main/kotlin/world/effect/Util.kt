ae4b8651896b67ab90557e80879f0513bcd92745
Alec West <alecwest38@gmail.com>
2018-07-11 19:50:47 -0400

    getAssociatedWorldEffects should be static

--- src/main/kotlin/world/effect/Util.kt
+++ src/main/kotlin/world/effect/Util.kt
@@ -3,20 +3,22 @@ package world.effect
 import world.RoomContent
 
 class Util {
-    fun getAssociatedWorldEffects(roomContent: RoomContent): ArrayList<WorldEffect> {
-        when(roomContent) {
-            RoomContent.BLOCKADE -> TODO()
-            RoomContent.BREEZE -> TODO()
-            RoomContent.BUMP -> TODO()
-            RoomContent.FOOD -> TODO()
-            RoomContent.GLITTER -> TODO()
-            RoomContent.GOLD -> TODO()
-            RoomContent.MOO -> TODO()
-            RoomContent.PIT -> TODO()
-            RoomContent.STENCH -> TODO()
-            RoomContent.SUPMUW_EVIL -> TODO()
-            RoomContent.SUPMUW -> TODO()
-            RoomContent.WUMPUS -> TODO()
+    companion object {
+        fun getAssociatedWorldEffects(roomContent: RoomContent): ArrayList<WorldEffect> {
+            when(roomContent) {
+                RoomContent.BLOCKADE -> TODO()
+                RoomContent.BREEZE -> TODO()
+                RoomContent.BUMP -> TODO()
+                RoomContent.FOOD -> TODO()
+                RoomContent.GLITTER -> TODO()
+                RoomContent.GOLD -> TODO()
+                RoomContent.MOO -> TODO()
+                RoomContent.PIT -> TODO()
+                RoomContent.STENCH -> TODO()
+                RoomContent.SUPMUW_EVIL -> TODO()
+                RoomContent.SUPMUW -> TODO()
+                RoomContent.WUMPUS -> TODO()
+            }
         }
     }
 }
