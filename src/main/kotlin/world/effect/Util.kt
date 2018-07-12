23650e3af6cece6adba8b9a22a0a5105156a2696
Alec West <alecwest38@gmail.com>
2018-07-11 20:35:35 -0400

    Add failing parameterized test for AddHereEffect

--- src/main/kotlin/world/effect/Util.kt
+++ src/main/kotlin/world/effect/Util.kt
@@ -5,20 +5,22 @@ import world.RoomContent
 class Util {
     companion object {
         fun getAssociatedWorldEffects(roomContent: RoomContent): ArrayList<WorldEffect> {
+            val worldEffects: ArrayList<WorldEffect>
             when(roomContent) {
-                RoomContent.BLOCKADE -> TODO()
-                RoomContent.BREEZE -> TODO()
-                RoomContent.BUMP -> TODO()
-                RoomContent.FOOD -> TODO()
-                RoomContent.GLITTER -> TODO()
-                RoomContent.GOLD -> TODO()
-                RoomContent.MOO -> TODO()
-                RoomContent.PIT -> TODO()
-                RoomContent.STENCH -> TODO()
-                RoomContent.SUPMUW_EVIL -> TODO()
-                RoomContent.SUPMUW -> TODO()
-                RoomContent.WUMPUS -> TODO()
+                RoomContent.BLOCKADE -> worldEffects = arrayListOf()
+                RoomContent.BREEZE -> worldEffects = arrayListOf()
+                RoomContent.BUMP -> worldEffects = arrayListOf()
+                RoomContent.FOOD -> worldEffects = arrayListOf()
+                RoomContent.GLITTER -> worldEffects = arrayListOf()
+                RoomContent.GOLD -> worldEffects = arrayListOf()
+                RoomContent.MOO -> worldEffects = arrayListOf()
+                RoomContent.PIT -> worldEffects = arrayListOf()
+                RoomContent.STENCH -> worldEffects = arrayListOf()
+                RoomContent.SUPMUW_EVIL -> worldEffects = arrayListOf()
+                RoomContent.SUPMUW -> worldEffects = arrayListOf()
+                RoomContent.WUMPUS -> worldEffects = arrayListOf()
             }
+            return worldEffects
         }
     }
 }
--- src/test/kotlin/world/WorldTest.kt
+++ src/test/kotlin/world/WorldTest.kt
@@ -2,32 +2,45 @@ package world
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 import java.awt.Point
+import java.util.stream.Stream
 
 // TODO use @ParameterizedTest and @MethodSource
 class WorldTest {
-    private val size: Int = 2
-    private val world: World = World(size)
-
-    init {
-        world.addRoomContent(Point(size - 1, size - 1), RoomContent.STENCH)
-        world.addRoomContent(Point(size - 1, size - 1), RoomContent.BREEZE)
-    }
-
     @Test
     fun `verify world init`() {
-        assertEquals(size * size, world.rooms.size)
+        val world = World(2)
+        assertEquals(4, world.rooms.size)
     }
 
-    @Test
-    fun `add content to room`() {
-        val initialNumberItems = world.rooms[world.getRoomIndex(Point(1, 1))].roomContent.size
-        world.addRoomContent(Point(1, 1), RoomContent.FOOD)
-        assertEquals(initialNumberItems + 1, world.rooms[world.getRoomIndex(Point(1, 1))].roomContent.size)
+
+    @ParameterizedTest
+    @MethodSource("validAddRoomContentProvider")
+    fun `add content to room`(testData: ValidRoomContentTestData) {
+        val world = World(2)
+        for (roomContent in testData.contentToAddOrRemove) {
+            world.addRoomContent(Point(1, 1), roomContent)
+        }
+        assertEquals(testData.finalContent.size, world.rooms[world.getRoomIndex(Point(1, 1))].roomContent.size)
+        for (roomContent in testData.finalContent) {
+            assertTrue(world.hasRoomContent(Point(1, 1), roomContent))
+        }
     }
+
+    private fun validAddRoomContentProvider() = Stream.of(
+            ValidRoomContentTestData(arrayListOf(), arrayListOf(RoomContent.GOLD), arrayListOf(RoomContent.GOLD, RoomContent.GLITTER)),
+            ValidRoomContentTestData(arrayListOf(), arrayListOf(RoomContent.MOO), arrayListOf(RoomContent.MOO))
+    )
+
 
     @Test
     fun `remove content from room`() {
+        val world = World(2)
+        world.addRoomContent(Point(1, 1), RoomContent.BREEZE)
+        world.addRoomContent(Point(1, 1), RoomContent.STENCH)
+
         val initialNumberItems = world.rooms[world.getRoomIndex(Point(1, 1))].roomContent.size
         world.removeRoomContent(Point(1, 1), RoomContent.BREEZE)
         assertEquals(initialNumberItems - 1, world.rooms[world.getRoomIndex(Point(1, 1))].roomContent.size)
@@ -35,12 +48,17 @@ class WorldTest {
 
     @Test
     fun `check room for content`() {
+        val world = World(2)
+        world.addRoomContent(Point(1, 1), RoomContent.BREEZE)
+        world.addRoomContent(Point(1, 1), RoomContent.STENCH)
+
         assertTrue(world.hasRoomContent(Point(1, 1), RoomContent.BREEZE))
         assertFalse(world.hasRoomContent(Point(1, 1), RoomContent.FOOD))
     }
 
     @Test
     fun `get index of room`() {
+        val world = World(2)
         assertEquals(0, world.getRoomIndex(Point(0, 0)))
         assertEquals(1, world.getRoomIndex(Point(1, 0)))
         assertEquals(2, world.getRoomIndex(Point(0, 1)))
@@ -48,3 +66,9 @@ class WorldTest {
         assertEquals(-1, world.getRoomIndex(Point(2, 1)))
     }
 }
+
+data class ValidRoomContentTestData (
+        val initialContent: ArrayList<RoomContent>,
+        val contentToAddOrRemove: ArrayList<RoomContent>,
+        val finalContent: ArrayList<RoomContent>
+)
