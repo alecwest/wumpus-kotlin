1f37930540ba23e474dea0d1c5eb300475f68544
Alec West <alecwest38@gmail.com>
2018-07-13 18:05:17 -0400

    Add Point directional extensions

---
+++ src/main/kotlin/util/PointUtil.kt
@@ -0,0 +1,43 @@
+package util
+
+import java.awt.Point
+
+fun Point.north(): Point {
+    return Point(this.x + 1, this.y)
+}
+
+fun Point.east(): Point {
+    return Point(this.x, this.y + 1)
+}
+
+fun Point.south(): Point {
+    return Point(this.x - 1, this.y)
+}
+
+fun Point.west(): Point {
+    return Point(this.x, this.y - 1)
+}
+
+fun Point.northEast(): Point {
+    return Point(this.x + 1, this.y + 1)
+}
+
+fun Point.northWest(): Point {
+    return Point(this.x + 1, this.y - 1)
+}
+
+fun Point.southEast(): Point {
+    return Point(this.x - 1, this.y + 1)
+}
+
+fun Point.southWest(): Point {
+    return Point(this.x - 1, this.y - 1)
+}
+
+fun Point.adjacents(): ArrayList<Point> {
+    return arrayListOf(north(), east(), south(), west())
+}
+
+fun Point.diagonals(): ArrayList<Point> {
+    return arrayListOf(northEast(), northWest(), southEast(), southWest())
+}
