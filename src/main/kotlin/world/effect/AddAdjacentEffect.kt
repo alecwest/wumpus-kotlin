2be488116048929d1df75322128c7ceb7c119bc7
Alec West <alecwest38@gmail.com>
2018-07-13 18:14:56 -0400

    Implement adjacent and diagonal effects

--- src/main/kotlin/world/effect/AddAdjacentEffect.kt
+++ src/main/kotlin/world/effect/AddAdjacentEffect.kt
@@ -1,11 +1,14 @@
 package world.effect
 
+import util.adjacents
 import world.RoomContent
 import world.World
 import java.awt.Point
 
-class AddAdjacentEffect(roomContent: RoomContent): WorldEffect {
+class AddAdjacentEffect(private val roomContent: RoomContent): WorldEffect {
     override fun applyEffect(world: World, point: Point) {
-
+        for (adjacentPoint in point.adjacents()) {
+            world.addRoomContent(adjacentPoint, roomContent)
+        }
     }
 }
--- src/main/kotlin/world/effect/AddDiagonalEffect.kt
+++ src/main/kotlin/world/effect/AddDiagonalEffect.kt
@@ -1,11 +1,14 @@
 package world.effect
 
+import util.diagonals
 import world.RoomContent
 import world.World
 import java.awt.Point
 
-class AddDiagonalEffect(roomContent: RoomContent): WorldEffect {
+class AddDiagonalEffect(private val roomContent: RoomContent): WorldEffect {
     override fun applyEffect(world: World, point: Point) {
-
+        for (diagonalPoint in point.diagonals()) {
+            world.addRoomContent(diagonalPoint, roomContent)
+        }
     }
 }
