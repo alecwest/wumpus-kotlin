974f0481253e68e8868b1f39f8979905c191fc22
Alec West <alecwest38@gmail.com>
2018-07-13 22:04:49 -0400

    Add AdjacentEffect test

---
+++ src/test/kotlin/world/effect/AddAdjacentEffectTest.kt
@@ -0,0 +1,25 @@
+package world.effect
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import util.adjacents
+import util.diagonals
+import world.RoomContent
+import world.World
+import java.awt.Point
+
+class AddAdjacentEffectTest {
+    private val world: World = World(3)
+    private val pointToAddTo: Point = Point(1, 1)
+
+    @Test
+    fun `add adjacent breeze effect to rooms`() {
+        world.addRoomContent(pointToAddTo, RoomContent.PIT)
+        for (point in pointToAddTo.adjacents()) {
+            assertTrue(world.hasRoomContent(point, RoomContent.BREEZE))
+        }
+        for (point in pointToAddTo.diagonals()) {
+            assertTrue(world.roomIsEmpty(point))
+        }
+    }
+}
