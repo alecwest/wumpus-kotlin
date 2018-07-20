bd9fd9b7b1e02b58fda5100dcd0f35f9bd6e9267
Alec West <alecwest38@gmail.com>
2018-07-20 19:30:30 -0400

    Use map.plus function

--- src/main/kotlin/server/command/GrabFoodCommand.kt
+++ src/main/kotlin/server/command/GrabFoodCommand.kt
@@ -8,9 +8,8 @@ import game.world.RoomContent
 class GrabFoodCommand(val game: Game): Command {
     override fun execute() {
         val playerLocation = game.gameState.player.getLocation()
-        val playerInventory = game.gameState.player.getInventory().toMutableMap()
-        playerInventory[Inventory.FOOD] = 1
         if (game.gameState.world.hasRoomContent(playerLocation, RoomContent.FOOD)) {
+            val playerInventory = game.gameState.player.getInventory().plus(Pair(Inventory.FOOD, 1))
             game.gameState.world.removeRoomContent(playerLocation, RoomContent.FOOD)
             game.gameState.player.playerState = game.gameState.player.playerState.copyThis(
                     inventory = PlayerInventory(playerInventory.toMap())
