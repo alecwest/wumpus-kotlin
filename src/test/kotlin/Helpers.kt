67a705ce88b066424403cca8b4de758545e4cfae
Alec West <alecwest38@gmail.com>
2018-07-22 09:54:47 -0400

    Add create player, world, game helpers

--- src/test/kotlin/Helpers.kt
+++ src/test/kotlin/Helpers.kt
@@ -1,12 +1,46 @@
+import game.Game
+import game.GameState
+import game.player.InventoryItem
+import game.player.Player
+import game.player.PlayerInventory
+import game.player.PlayerState
 import game.world.Room
 import game.world.RoomContent
+import game.world.World
 import org.junit.jupiter.api.Assertions.assertEquals
+import util.Direction
+import java.awt.Point
 
 class Helpers {
     companion object {
+        // TODO all of these create functions shouldn't just be test helpers
         fun createRoom(roomContent: ArrayList<RoomContent>
                        = arrayListOf(RoomContent.BREEZE, RoomContent.STENCH)): Room {
             return Room(roomContent)
+        }
+
+        fun createWorld(size: Int = 10,
+                        roomContent: Map<Point, RoomContent> =
+                                mapOf(Point(2, 2) to RoomContent.PIT)): World {
+            val world = World(size)
+            for (point in roomContent.keys) {
+                world.addRoomContent(point, roomContent.getValue(point))
+            }
+            return world
+        }
+
+        fun createPlayer(alive: Boolean = true,
+                         location: Point = Point(0, 0),
+                         facing: Direction = Direction.NORTH,
+                         inventoryContent: Map<InventoryItem, Int> =
+                                 mapOf(InventoryItem.ARROW to 2)): Player {
+            return Player(PlayerState(alive, location, facing, PlayerInventory(inventoryContent)))
+        }
+
+        fun createGame(active: Boolean = true,
+                       world: World = createWorld(),
+                       player: Player = createPlayer()): Game {
+            return Game(GameState(active, world, player))
         }
 
         fun assertContains(content: String, subString: String, numExpected: Int) {
