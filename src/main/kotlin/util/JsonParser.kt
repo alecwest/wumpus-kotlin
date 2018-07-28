fb5bdfb0a05ba0029fe76b4db5202c82c93c8e63
Alec West <alecwest38@gmail.com>
2018-07-28 19:25:52 -0400

    Rewrite buildFromJsonFile using Klaxon Object binding

--- src/main/kotlin/util/JsonParser.kt
+++ src/main/kotlin/util/JsonParser.kt
@@ -3,47 +3,39 @@ package util
 import com.beust.klaxon.*
 import game.Game
 import game.GameState
+import game.player.Player
+import game.player.PlayerState
+import game.player.toInventoryItem
 import game.world.World
 import game.world.WorldState
 import game.world.toRoomContent
 import java.awt.Point
+import java.io.File
 import java.io.FileReader
 
-class JsonParser {
-    data class Data(val x: Int, val y: Int, val content: List<String>)
+internal class JsonPlayer(val x: Int, val y: Int, val direction: String, val inventory: List<String>)
 
+internal class JsonRoomContent(val x: Int, val y: Int, val content: List<String>)
+
+internal class JsonWorld(val `world-size`: Int, val data: List<JsonRoomContent>, val player: JsonPlayer)
+
+class JsonParser {
     companion object {
         fun buildFromJsonFile(fileName: String): Game {
-            val jsonReader = JsonReader(FileReader(fileName))
-            val klaxon = Klaxon()
-            var world = World(0)
-
-            jsonReader.use {
-                it.beginObject {
-                    while (it.hasNext()) {
-                        val readName = it.nextName()
-                        when (readName) {
-                            "world-size" -> world = World(it.nextInt())
-                            "data" -> world.parseDataArray(klaxon, it)
-                        }
-                    }
+            val r = Klaxon().parse<JsonWorld>(File(fileName))
+            val world = World(size = r!!.`world-size`)
+            val player = Player(PlayerState(location = Point(r.player.x, r.player.y),
+                    facing = r.player.direction.toDirection()))
+            for (room in r.data) {
+                for (roomContentString in room.content) {
+                    world.addRoomContent(Point(room.x, room.y), roomContentString.toRoomContent())
                 }
             }
-            return Game(GameState(world = world))
-        }
-
-        private fun World.parseDataArray(klaxon: Klaxon, reader: JsonReader) {
-            reader.beginArray {
-                while (reader.hasNext()) {
-                    val data = klaxon.parse<Data>(reader)
-                    if (data != null) {
-                        for (contentChar in data.content) {
-                            val roomContent = contentChar.toRoomContent()
-                            this.addRoomContent(Point(data.x, data.y), roomContent)
-                        }
-                    }
-                }
+            for (inventoryItemString in r.player.inventory) {
+                player.addToInventory(inventoryItemString.toInventoryItem())
             }
+
+            return Game(GameState(world = world, player = player))
         }
     }
 }
