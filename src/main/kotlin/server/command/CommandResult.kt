6a32a2a26ce3d6362cb78b6d875647e5bfbc4130
Alec West <alecwest38@gmail.com>
2018-07-28 20:19:10 -0400

    Write CommandResult tests

--- src/main/kotlin/server/command/CommandResult.kt
+++ src/main/kotlin/server/command/CommandResult.kt
@@ -6,4 +6,5 @@ data class CommandResult(private val perceptions: ArrayList<Perception>) {
     fun getPerceptions() = perceptions
     fun blockadeHit() = getPerceptions().contains(Perception.BUMP)
     fun wallHit() = getPerceptions().contains(Perception.WALL)
+    fun moveRejected() = blockadeHit() || wallHit()
 }
--- src/test/kotlin/server/command/CommandResultTest.kt
+++ src/test/kotlin/server/command/CommandResultTest.kt
@@ -1,5 +1,31 @@
 package server.command
 
-internal class CommandResultTest {
+import game.Perception
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
+internal class CommandResultTest {
+    private val commandResult = CommandResult(arrayListOf(Perception.BUMP, Perception.MOO))
+
+    @Test
+    fun `get perceptions`() {
+        assertEquals(arrayListOf(Perception.BUMP, Perception.MOO), commandResult.getPerceptions())
+    }
+
+    @Test
+    fun `check blockade hit`() {
+        assertTrue(commandResult.blockadeHit())
+    }
+
+    @Test
+    fun `check wall hit`() {
+        assertFalse(commandResult.wallHit())
+    }
+
+    @Test
+    fun `check move rejected`() {
+        assertTrue(commandResult.moveRejected())
+    }
 }
