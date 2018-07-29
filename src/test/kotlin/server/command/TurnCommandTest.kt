2e9372f27a7fb041bc28a73ba05b583006bc14a9
Alec West <alecwest38@gmail.com>
2018-07-29 11:46:36 -0400

    Add CommandResult to TurnCommandTest

--- src/test/kotlin/server/command/TurnCommandTest.kt
+++ src/test/kotlin/server/command/TurnCommandTest.kt
@@ -4,6 +4,7 @@ import game.Game
 import game.GameState
 import game.player.Player
 import game.player.PlayerState
+import game.world.RoomContent
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
@@ -16,22 +17,23 @@ internal class TurnCommandTest {
     @MethodSource("validTurnCommandTestDataProvider")
     fun `execute turn commands`(testData: ValidTurnCommandTestData) {
         testData.command.execute()
-        assertEquals(testData.expectedDirection, initialGame.getPlayerDirection())
+        assertEquals(testData.expectedDirection, testData.givenGame.getPlayerDirection())
+        assertEquals(testData.expectedCommandResult, testData.givenGame.getCommandResult())
         // Verify the rest of the player state is maintained
-        assertEquals(Point(2, 2), initialGame.getPlayerLocation())
+        assertEquals(Point(2, 2), testData.givenGame.getPlayerLocation())
     }
 
     companion object {
-        private val initialGame = Game(GameState(player =
-        Player(playerState =
-        PlayerState(location = Point(2, 2), facing = Direction.SOUTH))))
+        private val initialGame = Helpers.createGame(player =
+        Helpers.createPlayer(location = Point(2, 2), facing = Direction.SOUTH),
+                world = Helpers.createWorld(roomContent = mapOf(Point(3, 2) to RoomContent.PIT)))
 
         @JvmStatic
         fun validTurnCommandTestDataProvider() = Stream.of(
-                ValidTurnCommandTestData(initialGame, TurnCommand(initialGame, Direction.EAST), Direction.EAST),
-                ValidTurnCommandTestData(initialGame, TurnCommand(initialGame, Direction.NORTH), Direction.NORTH),
-                ValidTurnCommandTestData(initialGame, TurnCommand(initialGame, Direction.EAST), Direction.EAST),
-                ValidTurnCommandTestData(initialGame, TurnCommand(initialGame, Direction.SOUTH), Direction.SOUTH)
+                ValidTurnCommandTestData(initialGame, TurnCommand(initialGame, Direction.EAST), Direction.EAST, CommandResult()),
+                ValidTurnCommandTestData(initialGame, TurnCommand(initialGame, Direction.NORTH), Direction.NORTH, CommandResult()),
+                ValidTurnCommandTestData(initialGame, TurnCommand(initialGame, Direction.EAST), Direction.EAST, CommandResult()),
+                ValidTurnCommandTestData(initialGame, TurnCommand(initialGame, Direction.SOUTH), Direction.SOUTH, CommandResult())
         )
     }
 }
@@ -39,5 +41,6 @@ internal class TurnCommandTest {
 data class ValidTurnCommandTestData (
         val givenGame: Game,
         val command: Command,
-        val expectedDirection: Direction
+        val expectedDirection: Direction,
+        val expectedCommandResult: CommandResult
 )
