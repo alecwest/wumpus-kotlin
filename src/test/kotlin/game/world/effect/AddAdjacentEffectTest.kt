6775b20d8c52e32d4cf5e94365f32879d3483565
alecwest <alecwest38@gmail.com>
2018-07-30 13:26:19 -0400

    Add removeRoomContent test to AdjacentEffect

--- src/test/kotlin/game/world/effect/AddAdjacentEffectTest.kt
+++ src/test/kotlin/game/world/effect/AddAdjacentEffectTest.kt
@@ -6,21 +6,30 @@ import util.adjacents
 import util.diagonals
 import game.world.RoomContent
 import game.world.World
-import game.world.WorldState
 import java.awt.Point
+import kotlin.test.assertFalse
 
 class AddAdjacentEffectTest {
     private val world: World = World(3)
-    private val pointToAddTo: Point = Point(1, 1)
+    private val point: Point = Point(1, 1)
 
     @Test
     fun `add breeze to adjacent rooms as effect of pit`() {
-        world.addRoomContent(pointToAddTo, RoomContent.PIT)
-        for (point in pointToAddTo.adjacents()) {
+        world.addRoomContent(point, RoomContent.PIT)
+        for (point in point.adjacents()) {
             assertTrue(world.hasRoomContent(point, RoomContent.BREEZE))
         }
-        for (point in pointToAddTo.diagonals()) {
+        for (point in point.diagonals()) {
             assertTrue(world.roomIsEmpty(point))
+        }
+    }
+
+    @Test
+    fun `remove breeze from adjacent rooms as result of pit removal`() {
+        val world = Helpers.createWorld(roomContent = mapOf(point to arrayListOf(RoomContent.PIT)))
+        world.removeRoomContent(point, RoomContent.PIT)
+        for (point in point.adjacents() + point.diagonals()) {
+            assertFalse(world.hasRoomContent(point, RoomContent.BREEZE))
         }
     }
 }
