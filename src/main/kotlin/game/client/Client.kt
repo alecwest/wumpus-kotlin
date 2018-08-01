a5987171a2ba044ea13faf7be18fd27aca3f0104
Alec West <alecwest38@gmail.com>
2018-08-01 08:24:53 -0400

    Add Client initialization

--- src/main/kotlin/game/client/Client.kt
+++ src/main/kotlin/game/client/Client.kt
@@ -1,8 +1,14 @@
 package game.client
 
+import game.server.Server
+
 /**
  * Client acts as the interface for the agent (human or AI) to communicate with the Server
  */
-class Client {
+class Client(fileName: String = "", worldSize: Int = 10) {
+    internal var sessionId = requestNewSessionId(fileName, worldSize)
 
+    private fun requestNewSessionId(fileName: String, worldSize: Int): Int {
+        return Server.newSession(fileName, worldSize)
+    }
 }
--- src/test/kotlin/game/client/ClientTest.kt
+++ src/test/kotlin/game/client/ClientTest.kt
@@ -1,7 +1,12 @@
 package game.client
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 internal class ClientTest {
-
+    @Test
+    fun `initialize Client session`() {
+        assertEquals(0, Client().sessionId)
+        assertEquals(1, Client().sessionId)
+    }
 }
