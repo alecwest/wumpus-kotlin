977b1fc8543bdeb5ddccd8d8d6641005cafbca13
Alec West <alecwest38@gmail.com>
2018-08-02 19:31:32 -0400

    Add player state and room content to CommandResult

--- src/main/kotlin/game/command/CommandResult.kt
+++ src/main/kotlin/game/command/CommandResult.kt
@@ -1,9 +1,15 @@
 package game.command
 
+import game.player.PlayerState
 import game.world.Perception
+import game.world.RoomContent
 
-data class CommandResult(private val perceptions: ArrayList<Perception> = arrayListOf()) {
+data class CommandResult(private val perceptions: ArrayList<Perception> = arrayListOf(),
+                         private val playerState: PlayerState = PlayerState(),
+                         private val roomContent: ArrayList<RoomContent>) {
     fun getPerceptions() = perceptions
+    fun getPlayerState() = playerState
+    fun getRoomContent() = roomContent
     fun blockadeHit() = getPerceptions().contains(Perception.BLOCKADE_BUMP)
     fun wallHit() = getPerceptions().contains(Perception.WALL_BUMP)
     fun moveRejected() = blockadeHit() || wallHit()
--- src/test/kotlin/game/command/CommandResultTest.kt
+++ src/test/kotlin/game/command/CommandResultTest.kt
@@ -1,17 +1,31 @@
 package game.command
 
+import game.player.PlayerState
 import game.world.Perception
+import game.world.RoomContent
 import org.junit.jupiter.api.Test
+import java.awt.Point
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 internal class CommandResultTest {
-    private val commandResult = CommandResult(arrayListOf(Perception.BLOCKADE_BUMP, Perception.MOO))
+    private val commandResult = CommandResult(arrayListOf(Perception.BLOCKADE_BUMP, Perception.MOO),
+            PlayerState(location = Point(44, 23)), arrayListOf(RoomContent.ARROW))
 
     @Test
     fun `get perceptions`() {
         assertEquals(arrayListOf(Perception.BLOCKADE_BUMP, Perception.MOO), commandResult.getPerceptions())
+    }
+
+    @Test
+    fun `get player state`() {
+        assertEquals(PlayerState(location = Point(44, 23)).getLocation(), commandResult.getPlayerState().getLocation())
+    }
+
+    @Test
+    fun `get room content`() {
+        assertEquals(arrayListOf(RoomContent.ARROW).toString(), commandResult.getRoomContent().toString())
     }
 
     @Test
