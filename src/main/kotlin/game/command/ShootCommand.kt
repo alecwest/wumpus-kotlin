f1fa46c39131c9a2498df063743389585aca92c4
Alec West <alecwest38@gmail.com>
2018-08-03 12:20:21 -0400

    Small ShootCommand refactor

--- src/main/kotlin/game/command/ShootCommand.kt
+++ src/main/kotlin/game/command/ShootCommand.kt
@@ -4,27 +4,40 @@ import game.player.InventoryItem
 import game.world.Perception
 import game.world.RoomContent
 import util.adjacent
+import java.awt.Point
 
 class ShootCommand: Command() {
+    private val perceptionList = arrayListOf<Perception>()
+
     override fun execute() {
-        val perceptionList = arrayListOf<Perception>()
         if (game.playerHasItem(InventoryItem.ARROW)) {
             var currentRoom = game.getPlayerLocation().adjacent(game.getPlayerDirection())
             game.removeFromPlayerInventory(InventoryItem.ARROW)
-            while (game.roomIsValid(currentRoom)) {
+            loop@ while (game.roomIsValid(currentRoom)) {
                 val roomContent = game.getRoomContent(currentRoom)
-                if (roomContent.contains(RoomContent.SUPMUW) ||
-                        roomContent.contains(RoomContent.SUPMUW_EVIL) ||
-                        roomContent.contains(RoomContent.WUMPUS)) {
-                    game.removeFromRoom(currentRoom, RoomContent.SUPMUW)
-                    game.removeFromRoom(currentRoom, RoomContent.WUMPUS)
-                    game.removeFromRoom(currentRoom, RoomContent.SUPMUW_EVIL)
-                    perceptionList.add(Perception.SCREAM)
-                    break
+
+                when {
+                    roomContent.contains(RoomContent.SUPMUW) -> {
+                        kill(currentRoom, RoomContent.SUPMUW)
+                        break@loop
+                    }
+                    roomContent.contains(RoomContent.SUPMUW_EVIL) -> {
+                        kill(currentRoom, RoomContent.SUPMUW_EVIL)
+                        break@loop
+                    }
+                    roomContent.contains(RoomContent.WUMPUS) -> {
+                        kill(currentRoom, RoomContent.WUMPUS)
+                        break@loop
+                    }
                 }
                 currentRoom = currentRoom.adjacent(game.getPlayerDirection())
             }
         }
         game.setCommandResult(createCommandResult(perceptionList))
+    }
+
+    private fun kill(room: Point, roomContent: RoomContent) {
+        game.removeFromRoom(room, roomContent)
+        perceptionList.add(Perception.SCREAM)
     }
 }
