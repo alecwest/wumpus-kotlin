0699cc9d719173f0d6dad4d3c443804bacccfd3f
Alec West <alecwest38@gmail.com>
2018-08-04 08:01:35 -0400

    Parameterize Intelligence test

--- src/test/kotlin/game/agent/intelligence/IntelligenceTest.kt
+++ src/test/kotlin/game/agent/intelligence/IntelligenceTest.kt
@@ -1,20 +1,39 @@
 package game.agent.intelligence
 
 import Helpers.Companion.assertCommandEquals
+import game.command.Command
 import game.command.CommandResult
 import game.command.MoveCommand
 import game.world.RoomContent
-import org.junit.jupiter.api.Test
+import game.world.World
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 import java.awt.Point
+import java.util.stream.Stream
 
 internal class IntelligenceTest {
 
-    @Test
-    fun `use basic intelligence`() {
+    @ParameterizedTest
+    @MethodSource("validIntelligenceTestDataProvider")
+    fun `use basic intelligence`(testData: ValidIntelligenceTestData) {
+        assertCommandEquals(MoveCommand(), testData.givenIntelligence.chooseNextMove(testData.givenWorld, testData.givenCommandResult))
+    }
+
+    companion object {
         val world = Helpers.createWorld(
                 roomContent = mapOf(Point(0, 4) to arrayListOf(RoomContent.BLOCKADE)))
-        val intelligence = BasicIntelligence()
         val commandResult = CommandResult()
-        assertCommandEquals(MoveCommand(), intelligence.chooseNextMove(world, commandResult))
+
+        @JvmStatic
+        fun validIntelligenceTestDataProvider() = Stream.of(
+                ValidIntelligenceTestData(world, BasicIntelligence(), commandResult, MoveCommand())
+        )
     }
 }
+
+data class ValidIntelligenceTestData (
+    val givenWorld: World,
+    val givenIntelligence: Intelligence,
+    val givenCommandResult: CommandResult,
+    val expectedCommand: Command
+)
