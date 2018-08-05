10bee986bd81a5429e114c9847a3c21a9d9ba5a5
Alec West <alecwest38@gmail.com>
2018-08-05 07:23:27 -0400

    Clean up imports

--- src/test/kotlin/game/agent/intelligence/BasicIntelligenceTest.kt
+++ src/test/kotlin/game/agent/intelligence/BasicIntelligenceTest.kt
@@ -1,9 +1,6 @@
 package game.agent.intelligence
 
-import game.command.Command
-import game.command.CommandResult
-import game.command.MoveCommand
-import game.command.TurnCommand
+import game.command.*
 import game.world.Perception
 import game.world.World
 import org.junit.jupiter.api.Assertions
--- src/test/kotlin/game/agent/intelligence/IntelligenceTest.kt
+++ src/test/kotlin/game/agent/intelligence/IntelligenceTest.kt
@@ -1,20 +1,10 @@
 package game.agent.intelligence
 
-import game.command.Command
 import game.command.CommandResult
-import game.command.MoveCommand
-import game.command.TurnCommand
-import game.world.Perception
 import game.world.RoomContent
-import game.world.World
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.MethodSource
-import util.Direction
 import java.awt.Point
-import java.io.ByteArrayInputStream
-import java.util.stream.Stream
 
 internal class IntelligenceTest {
     @Test
