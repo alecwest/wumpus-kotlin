056f88d491c4807d8db7721b065ea09fbf4fee43
Alec West <alecwest38@gmail.com>
2018-08-08 11:09:37 -0400

    Reset room's objects on each process of last move

--- src/main/kotlin/game/agent/intelligence/Intelligence.kt
+++ src/main/kotlin/game/agent/intelligence/Intelligence.kt
@@ -4,6 +4,7 @@ import game.command.Command
 import game.command.CommandResult
 import game.command.MoveCommand
 import game.world.World
+import game.world.gameObjectValues
 
 abstract class Intelligence {
     open fun chooseNextMove(world: World, commandResult: CommandResult): Command {
@@ -12,8 +13,15 @@ abstract class Intelligence {
     }
 
     open fun processLastMove(world: World, commandResult: CommandResult) {
+        resetRoom(world, commandResult)
         for (gameObject in commandResult.getGameObjects()) {
             world.addGameObject(commandResult.getPlayerState().getLocation(), gameObject)
+        }
+    }
+
+    private fun resetRoom(world: World, commandResult: CommandResult) {
+        for (gameObject in gameObjectValues()) {
+            world.removeGameObject(commandResult.getPlayerState().getLocation(), gameObject)
         }
     }
 }
--- src/test/kotlin/game/agent/intelligence/IntelligenceTest.kt
+++ src/test/kotlin/game/agent/intelligence/IntelligenceTest.kt
@@ -1,21 +1,29 @@
 package game.agent.intelligence
 
 import game.command.CommandResult
+
 import game.world.GameObject
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import java.awt.Point
-
 internal class IntelligenceTest {
+    val intelligence = BasicIntelligence()
+    val lastMove = Helpers.createCommandResult(arrayListOf(),
+            Helpers.createPlayerState(location = Point(4, 4)),
+            arrayListOf(GameObject.BREEZE, GameObject.GLITTER, GameObject.GOLD))
+
     @Test
     fun `process last move with base method`() {
-        val lastMove = Helpers.createCommandResult(arrayListOf(),
-                Helpers.createPlayerState(location = Point(4, 4)),
-                arrayListOf(GameObject.BREEZE))
-        val intelligence = BasicIntelligence()
-
         intelligence.processLastMove(world, lastMove)
-        assertTrue(world.hasGameObject(lastMove.getPlayerState().getLocation(), GameObject.BREEZE))
+        assertEquals(arrayListOf(GameObject.BREEZE, GameObject.GLITTER, GameObject.GOLD), world.getGameObjects(lastMove.getPlayerState().getLocation()))
+    }
+
+    @Test
+    fun `process removal of object(s) from room`() {
+        val lastMove2 = lastMove.copyThis(gameObjects = arrayListOf(GameObject.BREEZE))
+        intelligence.processLastMove(world, lastMove)
+        intelligence.processLastMove(world, lastMove2)
+        assertEquals(arrayListOf(GameObject.BREEZE), world.getGameObjects(lastMove.getPlayerState().getLocation()))
     }
 
     companion object {
