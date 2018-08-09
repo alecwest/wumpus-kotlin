fc880254638d8c77dbd6cfe3772ca1d6625a8295
Alec West <alecwest38@gmail.com>
2018-08-09 07:37:35 -0400

    Make superclass of Intelligence

--- src/main/kotlin/game/agent/intelligence/KnowledgeBasedIntelligence.kt
+++ src/main/kotlin/game/agent/intelligence/KnowledgeBasedIntelligence.kt
@@ -1,5 +1,17 @@
 package game.agent.intelligence
 
-class KnowledgeBasedIntelligence {
+import game.command.Command
+import game.command.CommandResult
+import game.world.GameObject
+import game.world.World
+import java.awt.Point
 
+class KnowledgeBasedIntelligence: Intelligence() {
+    private val knowns: Map<Point, Set<GameObject>> = mapOf()
+    private val possibles: Map<Point, Set<GameObject>> = mapOf()
+
+    override fun chooseNextMove(world: World, commandResult: CommandResult): Command {
+
+        return super.chooseNextMove(world, commandResult)
+    }
 }
