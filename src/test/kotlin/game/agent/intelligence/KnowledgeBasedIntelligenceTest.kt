e455b9ca66c664157f6fd57d4d0963bc185dc229
Alec West <alecwest38@gmail.com>
2018-08-09 08:00:20 -0400

    Write failing test

--- src/test/kotlin/game/agent/intelligence/KnowledgeBasedIntelligenceTest.kt
+++ src/test/kotlin/game/agent/intelligence/KnowledgeBasedIntelligenceTest.kt
@@ -1,15 +1,24 @@
 package game.agent.intelligence
 
 import game.agent.Agent
+import game.agent.intelligence.IntelligenceTest.Companion.world
 import game.client.Client
+import game.world.GameObject
+import game.world.Perception
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import java.awt.Point
 
 internal class KnowledgeBasedIntelligenceTest {
-    val agent = Agent(Client(Helpers.testFilePath + "knowledgeBasedIntelligenceTestFile.json"), KnowledgeBasedIntelligence())
+    val intelligence = KnowledgeBasedIntelligence()
 
     @Test
     fun `process last move with knowns and possibles`() {
-
+        val lastMove = Helpers.createCommandResult(arrayListOf(Perception.BREEZE),
+                Helpers.createPlayerState(location = Point(0, 1)),
+                arrayListOf(GameObject.BREEZE))
+        intelligence.processLastMove(world, lastMove)
+        assertEquals(mapOf(Point(0, 2) to setOf(GameObject.PIT), Point(1, 1) to setOf(GameObject.PIT)), intelligence.possibles)
+        assertEquals(emptyMap<Point, Set<GameObject>>(), intelligence.knowns)
     }
 }
