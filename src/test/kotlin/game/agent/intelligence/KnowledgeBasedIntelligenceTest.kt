26aa2fd9859ed34870b63e5fe7ec40f54585277d
Alec West <alecwest38@gmail.com>
2018-08-09 07:46:51 -0400

    Init test agent and test

--- src/test/kotlin/game/agent/intelligence/KnowledgeBasedIntelligenceTest.kt
+++ src/test/kotlin/game/agent/intelligence/KnowledgeBasedIntelligenceTest.kt
@@ -1,5 +1,15 @@
 package game.agent.intelligence
 
+import game.agent.Agent
+import game.client.Client
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-internal class KnowledgeBasedIntelligenceTest
+internal class KnowledgeBasedIntelligenceTest {
+    val agent = Agent(Client(Helpers.testFilePath + "knowledgeBasedIntelligenceTestFile.json"), KnowledgeBasedIntelligence())
+
+    @Test
+    fun `process last move with knowns and possibles`() {
+
+    }
+}
