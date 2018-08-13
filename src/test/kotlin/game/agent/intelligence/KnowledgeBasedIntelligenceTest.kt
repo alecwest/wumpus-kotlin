25b48195289bd83cccf84118f20977a849c12ce4
Alec West <alecwest38@gmail.com>
2018-08-13 14:29:11 -0500

    Add test for processing MOO

--- src/test/kotlin/game/agent/intelligence/KnowledgeBasedIntelligenceTest.kt
+++ src/test/kotlin/game/agent/intelligence/KnowledgeBasedIntelligenceTest.kt
@@ -6,20 +6,41 @@ import game.world.GameObject
 import game.world.Perception
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 import java.awt.Point
+import java.util.stream.Stream
 
 internal class KnowledgeBasedIntelligenceTest {
     val intelligence = KnowledgeBasedIntelligence()
 
-    @Test
-    fun `process last move with knowns and possibles`() {
+    @ParameterizedTest
+    @MethodSource("validMoveProcessingTestDataProvider")
+    fun `process last move with knowns and possibles`(testData: ValidMoveProcessingTestData) {
+        intelligence.processLastMove(world, testData.lastMove)
+        assertEquals(testData.expectedPossibles, intelligence.possibles)
+        assertEquals(testData.expectedKnowns, intelligence.knowns)
+    }
+
+    companion object {
         val lastMove = Helpers.createCommandResult(arrayListOf(Perception.BREEZE),
                 Helpers.createPlayerState(location = Point(0, 1)))
-        intelligence.processLastMove(world, lastMove)
-        assertEquals(mapOf(Point(0, 0) to setOf(GameObject.PIT),
-                Point(0, 2) to setOf(GameObject.PIT),
-                Point(1, 1) to setOf(GameObject.PIT)), intelligence.possibles)
-        assertEquals(mapOf(Point(0, 1) to setOf(GameObject.BREEZE)), intelligence.knowns)
+
+        @JvmStatic
+        fun validMoveProcessingTestDataProvider() = Stream.of(
+                ValidMoveProcessingTestData(lastMove,
+                        mutableMapOf(Point(0, 0) to mutableSetOf<GameObject>(GameObject.PIT),
+                        Point(0, 2) to mutableSetOf<GameObject>(GameObject.PIT),
+                        Point(1, 1) to mutableSetOf<GameObject>(GameObject.PIT)),
+                        mutableMapOf(Point(0, 1) to mutableSetOf<GameObject>(GameObject.BREEZE))),
+                ValidMoveProcessingTestData(lastMove.copyThis(arrayListOf(Perception.MOO)),
+                        mutableMapOf(Point(0, 0) to mutableSetOf<GameObject>(GameObject.SUPMUW),
+                        Point(0, 2) to mutableSetOf<GameObject>(GameObject.SUPMUW),
+                        Point(1, 2) to mutableSetOf<GameObject>(GameObject.SUPMUW),
+                        Point(1, 0) to mutableSetOf<GameObject>(GameObject.SUPMUW),
+                        Point(1, 1) to mutableSetOf<GameObject>(GameObject.SUPMUW)),
+                        mutableMapOf(Point(0, 1) to mutableSetOf<GameObject>(GameObject.MOO)))
+        )
     }
 
     @Test
@@ -36,3 +57,9 @@ internal class KnowledgeBasedIntelligenceTest {
                 Point(0, 1) to setOf(GameObject.BREEZE)), intelligence.knowns)
     }
 }
+
+data class ValidMoveProcessingTestData (
+        val lastMove: CommandResult,
+        val expectedPossibles: MutableMap<Point, MutableSet<GameObject>>,
+        val expectedKnowns: MutableMap<Point, MutableSet<GameObject>>
+)
