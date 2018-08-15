9ba713351ee51b4d80b18d43cd6a8fe51c299b69
Alec West <alecwest38@gmail.com>
2018-08-15 10:29:45 -0500

    Move to own file

---
+++ src/main/kotlin/game/agent/intelligence/GameObjectMap.kt
@@ -0,0 +1,22 @@
+package game.agent.intelligence
+
+import game.world.GameObject
+import java.awt.Point
+
+data class GameObjectMap(internal val gameObjectMap: MutableMap<Point, MutableSet<GameObject>> = mutableMapOf()) {
+    fun add(point: Point, gameObject: GameObject) {
+        val gameObjects = gameObjectMap.getOrDefault(point, mutableSetOf())
+        gameObjects.add(gameObject)
+        gameObjectMap[point] = gameObjects
+    }
+
+    fun remove(point: Point, gameObject: GameObject) {
+        val gameObjects = gameObjectMap.getOrDefault(point, mutableSetOf())
+        gameObjects.remove(gameObject)
+        if (gameObjects.isEmpty()) {
+            gameObjectMap.remove(point)
+        } else {
+            gameObjectMap[point] = gameObjects
+        }
+    }
+}
--- src/main/kotlin/game/agent/intelligence/KnowledgeBasedIntelligence.kt
+++ src/main/kotlin/game/agent/intelligence/KnowledgeBasedIntelligence.kt
@@ -163,21 +163,3 @@ class KnowledgeBasedIntelligence: Intelligence() {
         return super.chooseNextMove(world, commandResult)
     }
 }
-
-data class GameObjectMap(internal val gameObjectMap: MutableMap<Point, MutableSet<GameObject>> = mutableMapOf()) {
-    fun add(point: Point, gameObject: GameObject) {
-        val gameObjects = gameObjectMap.getOrDefault(point, mutableSetOf())
-        gameObjects.add(gameObject)
-        gameObjectMap[point] = gameObjects
-    }
-
-    fun remove(point: Point, gameObject: GameObject) {
-        val gameObjects = gameObjectMap.getOrDefault(point, mutableSetOf())
-        gameObjects.remove(gameObject)
-        if (gameObjects.isEmpty()) {
-            gameObjectMap.remove(point)
-        } else {
-            gameObjectMap[point] = gameObjects
-        }
-    }
-}
