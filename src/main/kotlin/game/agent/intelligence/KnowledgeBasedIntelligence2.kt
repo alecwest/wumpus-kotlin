d8bf387d826be06f35dab408157f91a6618260a7
Alec West <alecwest38@gmail.com>
2018-08-17 08:38:03 -0400

    Add facts for current location

--- src/main/kotlin/game/agent/intelligence/KnowledgeBasedIntelligence2.kt
+++ src/main/kotlin/game/agent/intelligence/KnowledgeBasedIntelligence2.kt
@@ -4,6 +4,8 @@ import game.command.Command
 import game.command.CommandResult
 import game.world.GameObject
 import game.world.World
+import game.world.gameObjectValues
+import game.world.toGameObjects
 import java.awt.Point
 
 class KnowledgeBasedIntelligence2 : Intelligence() {
@@ -15,5 +17,13 @@ class KnowledgeBasedIntelligence2 : Intelligence() {
 
     override fun processLastMove(world: World, commandResult: CommandResult) {
         super.processLastMove(world, commandResult)
+        val perceivedObjects = toGameObjects(commandResult.getPerceptions().toSet()) // TODO change perceptions to a set
+        val playerLocation = commandResult.getPlayerState().getLocation()
+        for (gameObject in gameObjectValues()) {
+            facts.addFact(
+                    playerLocation,
+                    if (perceivedObjects.contains(gameObject)) Fact.HAS else Fact.HAS_NO,
+                    gameObject)
+        }
     }
 }
