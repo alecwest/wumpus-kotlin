c514aa6f636ee451a354a4abd93ea622b47682a8
Alec West <alecwest38@gmail.com>
2018-08-18 19:48:55 -0500

    Remove roomIsSafe from Intelligence class

--- src/main/kotlin/game/agent/intelligence/KnowledgeBasedIntelligence2.kt
+++ src/main/kotlin/game/agent/intelligence/KnowledgeBasedIntelligence2.kt
@@ -22,10 +22,6 @@ class KnowledgeBasedIntelligence2 : Intelligence() {
         return MoveCommand()
     }
 
-    internal fun roomIsSafe(): Boolean {
-        return true
-    }
-
     override fun processLastMove(world: World, commandResult: CommandResult) {
         super.processLastMove(world, commandResult)
         this.world = world
