591455f77b9e52510f1ccf3fabaf50027405c0a5
Alec West <alecwest38@gmail.com>
2018-08-31 11:09:56 -0500

    Set gameActive on ExitCommand to false only if room has Exit

--- src/main/kotlin/game/command/ExitCommand.kt
+++ src/main/kotlin/game/command/ExitCommand.kt
@@ -1,9 +1,14 @@
 package game.command
 
+import game.world.GameObject;
+
 class ExitCommand: Command() {
     override fun execute() {
         game?.let { game ->
             game.setPlayerScore(game.getScore() + getMoveCost(game.getPlayerState()))
+            if (game.hasGameObject(game.getPlayerLocation(), GameObject.EXIT)) {
+                game.setActive(false)
+            }
             game.setCommandResult(createCommandResult())
         }
     }
--- src/test/kotlin/game/command/ExitCommandTest.kt
+++ src/test/kotlin/game/command/ExitCommandTest.kt
@@ -1,5 +1,7 @@
 package game.command
 
+import game.world.GameObject
+import game.world.Perception
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import java.awt.Point
@@ -15,12 +17,14 @@ internal class ExitCommandTest {
 
     @Test
     fun `execute exit command in exitable room`() {
-        val game = Helpers.createGame(player = Helpers.createPlayer(location = Point(0, 0)))
+        val game = Helpers.createGame(player = Helpers.createPlayer(location = Point(0, 0)),
+                world = Helpers.createWorld(gameObject = mapOf(Point(0, 0) to setOf(GameObject.EXIT))))
         val command = ExitCommand()
         command.setGame(game)
         command.execute()
-        assertEquals(Helpers.createCommandResult(
-                playerState = Helpers.createPlayerState(location = Point(0, 0), score = 1)), game.getCommandResult())
+        assertEquals(Helpers.createCommandResult(setOf(Perception.EXIT),
+                Helpers.createPlayerState(location = Point(0, 0), score = 1),
+                false), game.getCommandResult())
     }
 
     @Test
@@ -30,6 +34,7 @@ internal class ExitCommandTest {
         command.setGame(game)
         command.execute()
         assertEquals(Helpers.createCommandResult(
-                playerState = Helpers.createPlayerState(location = Point(1, 1), score = 1)), game.getCommandResult())
+                playerState = Helpers.createPlayerState(location = Point(1, 1), score = 1),
+                gameActive = true), game.getCommandResult())
     }
 }
