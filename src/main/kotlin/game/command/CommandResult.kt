17d0b8fe83dce095c7e823ac15f28e5562600c0f
Alec West <alecwest38@gmail.com>
2018-08-31 13:48:01 -0500

    LoD

--- src/main/kotlin/game/command/CommandResult.kt
+++ src/main/kotlin/game/command/CommandResult.kt
@@ -1,5 +1,6 @@
 package game.command
 
+import game.player.InventoryItem
 import game.player.PlayerState
 import game.world.Perception
 
@@ -8,7 +9,16 @@ data class CommandResult(private val perceptions: Set<Perception> = setOf(),
                          private val gameActive: Boolean = true) {
     fun getActive() = gameActive
     fun getPerceptions() = perceptions
+
     fun getPlayerState() = playerState
+    fun isAlive() = getPlayerState().isAlive()
+    fun getLocation() = getPlayerState().getLocation()
+    fun getDirection() = getPlayerState().getDirection()
+    fun getInventory() = getPlayerState().getInventory()
+    fun hasItem(item: InventoryItem) = getPlayerState().hasItem(item)
+    fun getNumberOf(item: InventoryItem) = getPlayerState().getNumberOf(item)
+    fun getScore() = getPlayerState().getScore()
+    
     fun blockadeHit() = getPerceptions().contains(Perception.BLOCKADE_BUMP)
     fun wallHit() = getPerceptions().contains(Perception.WALL_BUMP)
     fun moveRejected() = blockadeHit() || wallHit()
