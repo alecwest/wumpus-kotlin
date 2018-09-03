383ce4dda08eaf03cb51c28f981bcb1e24eea5a4
Alec West <alecwest38@gmail.com>
2018-09-03 17:42:34 -0500

    Move GameObjectFeature to own file

--- src/main/kotlin/game/world/GameObject.kt
+++ src/main/kotlin/game/world/GameObject.kt
@@ -1,11 +1,8 @@
 package game.world
 
-import game.Game
 import game.player.InventoryItem
 import game.world.GameObjectFeature.*
 import game.world.effect.*
-import util.adjacents
-import util.diagonals
 
 sealed class GameObject(val features: Set<GameObjectFeature> = setOf()) {
     object ARROW : GameObject(setOf(Shootable(10), Grabbable(InventoryItem.ARROW)))
@@ -82,42 +79,5 @@ fun Perception.toGameObject(): GameObject? {
 fun InventoryItem.toGameObject(): GameObject {
     return gameObjectsWithFeatures(setOf(Grabbable())).first {
         (it.getFeature(Grabbable()) as Grabbable).inventoryItem == this
-    }
-}
-
-sealed class GameObjectFeature {
-    class Blocking: GameObjectFeature() // For things that block a player from entering
-    open class Dangerous: GameObjectFeature() {
-        open fun killsPlayer(game: Game): Boolean {
-            return true
-        }
-    }
-    class ConditionallyDangerous(val proximityTo: GameObject): Dangerous() {
-        override fun killsPlayer(game: Game): Boolean {
-            val playerLocation = game.getPlayerLocation()
-            return (playerLocation.adjacents() + playerLocation.diagonals()).any {
-                game.hasGameObject(it, proximityTo)
-            }
-        }
-    }
-    class Destructable(val weaknesses: Set<GameObject> = setOf()): GameObjectFeature()
-    class Exitable: GameObjectFeature()
-    class Grabbable(val inventoryItem: InventoryItem? = null, val value: Int = 0): GameObjectFeature()
-    class Mappable(val character: String = ""): GameObjectFeature()
-    class Perceptable(val perception: Perception? = null): GameObjectFeature()
-    class RoomFilling: GameObjectFeature() // For things that must exist in a Room alone
-    class Shootable(val cost: Int = 1) : GameObjectFeature()
-    class WorldAffecting(val effects: ArrayList<WorldEffect> = arrayListOf()): GameObjectFeature() {
-        fun hasEffect(worldEffect: WorldEffect): Boolean {
-            return effects.any {
-                worldEffect::class == it::class && worldEffect.gameObject.toString() == it.gameObject.toString()
-            }
-        }
-
-        fun createsObject(gameObject: GameObject): Boolean {
-            return effects.any {
-                it.gameObject.toString() == gameObject.toString()
-            }
-        }
     }
 }
---
+++ src/main/kotlin/game/world/GameObjectFeature.kt
@@ -0,0 +1,44 @@
+package game.world
+
+import game.Game
+import game.player.InventoryItem
+import game.world.effect.WorldEffect
+import util.adjacents
+import util.diagonals
+
+sealed class GameObjectFeature {
+    class Blocking: GameObjectFeature() // For things that block a player from entering
+    open class Dangerous: GameObjectFeature() {
+        open fun killsPlayer(game: Game): Boolean {
+            return true
+        }
+    }
+    class ConditionallyDangerous(val proximityTo: GameObject): Dangerous() {
+        override fun killsPlayer(game: Game): Boolean {
+            val playerLocation = game.getPlayerLocation()
+            return (playerLocation.adjacents() + playerLocation.diagonals()).any {
+                game.hasGameObject(it, proximityTo)
+            }
+        }
+    }
+    class Destructable(val weaknesses: Set<GameObject> = setOf()): GameObjectFeature()
+    class Exitable: GameObjectFeature()
+    class Grabbable(val inventoryItem: InventoryItem? = null, val value: Int = 0): GameObjectFeature()
+    class Mappable(val character: String = ""): GameObjectFeature()
+    class Perceptable(val perception: Perception? = null): GameObjectFeature()
+    class RoomFilling: GameObjectFeature() // For things that must exist in a Room alone
+    class Shootable(val cost: Int = 1) : GameObjectFeature()
+    class WorldAffecting(val effects: ArrayList<WorldEffect> = arrayListOf()): GameObjectFeature() {
+        fun hasEffect(worldEffect: WorldEffect): Boolean {
+            return effects.any {
+                worldEffect::class == it::class && worldEffect.gameObject.toString() == it.gameObject.toString()
+            }
+        }
+
+        fun createsObject(gameObject: GameObject): Boolean {
+            return effects.any {
+                it.gameObject.toString() == gameObject.toString()
+            }
+        }
+    }
+}
