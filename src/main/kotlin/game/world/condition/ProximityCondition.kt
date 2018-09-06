aba88e4c18b5ebbcc3a47a46255f661e9d352cce
Alec West <alecwest38@gmail.com>
2018-09-05 20:46:06 -0500

    Override equals, hashcode, and toString of ProximityCondition

--- src/main/kotlin/game/world/condition/ProximityCondition.kt
+++ src/main/kotlin/game/world/condition/ProximityCondition.kt
@@ -20,4 +20,26 @@ class ProximityCondition(private val fact: Fact, private val thing: GameObject) 
             }
         }
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ProximityCondition
+
+        if (fact != other.fact) return false
+        if (thing != other.thing) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = fact.hashCode()
+        result = 31 * result + thing.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "ProximityCondition(fact=$fact, thing=$thing)"
+    }
 }
