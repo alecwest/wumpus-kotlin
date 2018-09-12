f548a12c7aa31a401fe081c8f86215e1979a3613
Alec West <alecwest38@gmail.com>
2018-09-12 16:04:40 -0500

    Test WorldEffect removeEffect method

--- src/test/kotlin/game/world/effect/WorldEffectTest.kt
+++ src/test/kotlin/game/world/effect/WorldEffectTest.kt
@@ -12,7 +12,7 @@ import java.util.stream.Stream
 
 internal class WorldEffectTest {
     @ParameterizedTest
-    @MethodSource("validWorldEffectTestDataProvider")
+    @MethodSource("validAddWorldEffectTestDataProvider")
     fun `apply affect`(testData: ValidWorldEffectTestData) {
         val world = testData.givenWorld
         val point = testData.givenPoint
@@ -23,9 +23,21 @@ internal class WorldEffectTest {
         }
     }
 
+    @ParameterizedTest
+    @MethodSource("validRemoveWorldEffectTestDataProvider")
+    fun `remove affect`(testData: ValidWorldEffectTestData) {
+        val world = testData.givenWorld
+        val point = testData.givenPoint
+        val effect = testData.givenWorldEffect
+        assertEquals(testData.expectedReturn, effect.removeEffect(world, point))
+        effect.roomsAffected(point).forEach { roomAffected ->
+            assertEquals(testData.effectObjectInAffectedRooms, world.hasGameObject(roomAffected, effect.gameObject))
+        }
+    }
+
     companion object {
         @JvmStatic
-        fun validWorldEffectTestDataProvider() = Stream.of(
+        fun validAddWorldEffectTestDataProvider() = Stream.of(
                 ValidWorldEffectTestData(Helpers.createWorld(), Point(1, 1),
                         AdjacentEffect(GameObject.GOLD),true, true),
                 ValidWorldEffectTestData(Helpers.createWorld(1), Point(0, 0),
@@ -43,6 +55,27 @@ internal class WorldEffectTest {
                 ValidWorldEffectTestData(Helpers.createWorld(), Point(1, 1),
                         ConditionalEffect(HereEffect(GameObject.GOLD), ProximityCondition(HAS, GameObject.PIT)),
                         false, false)
+        )
+
+        @JvmStatic
+        fun validRemoveWorldEffectTestDataProvider() = Stream.of(
+                ValidWorldEffectTestData(Helpers.createWorld(gameObject = mapOf(Point(1, 2) to setOf(GameObject.GOLD))),
+                        Point(1, 1), AdjacentEffect(GameObject.GOLD), true, false),
+                ValidWorldEffectTestData(Helpers.createWorld(1), Point(0, 0), AdjacentEffect(GameObject.GOLD),
+                        false, false),
+                ValidWorldEffectTestData(Helpers.createWorld(gameObject = mapOf(Point(2, 2) to setOf(GameObject.GOLD))),
+                        Point(1, 1), DiagonalEffect(GameObject.GOLD), true, false),
+                ValidWorldEffectTestData(Helpers.createWorld(1), Point(0, 0),
+                        DiagonalEffect(GameObject.GOLD), false, false),
+                ValidWorldEffectTestData(Helpers.createWorld(gameObject = mapOf(Point(1, 1) to setOf(GameObject.GOLD))),
+                        Point(1, 1), HereEffect(GameObject.GOLD), true, false),
+                ValidWorldEffectTestData(Helpers.createWorld(
+                        gameObject = mapOf(Point(2, 2) to setOf(GameObject.PIT))), Point(1, 1),
+                        ConditionalEffect(HereEffect(GameObject.GOLD), ProximityCondition(HAS, GameObject.PIT)),
+                        true, false),
+                ValidWorldEffectTestData(Helpers.createWorld(), Point(1, 1),
+                        ConditionalEffect(HereEffect(GameObject.GOLD), ProximityCondition(HAS, GameObject.PIT)),
+                        true, false)
         )
     }
 }
