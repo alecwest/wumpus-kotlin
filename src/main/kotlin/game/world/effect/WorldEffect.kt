6b9d5d0593c51b03292f6c8795b9df61e9ffc873
Alec West <alecwest38@gmail.com>
2018-09-16 09:56:15 -0500

    Small refactor

--- src/main/kotlin/game/world/effect/WorldEffect.kt
+++ src/main/kotlin/game/world/effect/WorldEffect.kt
@@ -18,9 +18,8 @@ abstract class WorldEffect(internal val gameObject: GameObject) {
     }
 
     open fun removeEffect(world: World, point: Point): Boolean {
-        val roomsAffected = roomsAffected(point).filter { world.roomIsValid(it) }
-        if (roomsAffected.all { !world.hasGameObject(it, gameObject) }) return false
-        if (roomsAffected.isEmpty()) return false
+        val roomsAffected = getRoomsAffected(world, point)
+        if (noEffectToRemove(world, point)) return false
         roomsAffected.forEach { affectedPoint ->
             if (!nearbyContentHasAssociatedEffect(world, affectedPoint)) {
                 world.removeGameObject(affectedPoint, gameObject)
@@ -31,6 +30,19 @@ abstract class WorldEffect(internal val gameObject: GameObject) {
     }
 
     abstract fun roomsAffected(point: Point): Set<Point>
+
+    internal fun noEffectToRemove(world: World, point: Point): Boolean {
+        val roomsAffected = getRoomsAffected(world, point)
+        if (roomsAffected.isEmpty() ||
+                roomsAffected.all { !world.hasGameObject(it, gameObject) }) {
+            return true
+        }
+        return false
+    }
+
+    internal fun getRoomsAffected(world: World, point: Point): List<Point> {
+        return roomsAffected(point).filter { world.roomIsValid(it) }
+    }
 
     fun nearbyContentHasAssociatedEffect(world: World, point: Point): Boolean {
         for (adjacentPoint in point.adjacents() + point.diagonals()) {
