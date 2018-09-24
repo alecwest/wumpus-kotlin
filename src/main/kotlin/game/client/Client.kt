25934fdfd63ba9b93b8f8e1a06043e854fa91995
Alec West <alecwest38@gmail.com>
2018-09-23 20:22:02 -0500

    Document Client

--- src/main/kotlin/game/client/Client.kt
+++ src/main/kotlin/game/client/Client.kt
@@ -5,21 +5,37 @@ import game.command.CommandResult
 import game.server.Server
 
 /**
+ * @param fileName file containing world info, or blank indicating a random world generation
+ * @param worldSize size of the world
+ *
  * Client acts as the interface for the agent (human or AI) to communicate with the Server
  */
 class Client(fileName: String = "", private val worldSize: Int = 10) {
     internal var sessionId = requestNewSessionId(fileName, worldSize)
 
+    /**
+     * @return [Int] id of a new session
+     *
+     * Create a new session with the Server
+     */
     private fun requestNewSessionId(fileName: String, worldSize: Int): Int {
         return Server.newSession(fileName, worldSize)
     }
 
     fun getWorldSize() = worldSize
 
+    /**
+     * @param command Command to be executed
+     *
+     * Send command to the Server
+     */
     fun makeMove(command: Command) {
         Server.makeMove(sessionId, command)
     }
 
+    /**
+     * @return [CommandResult] of the last move made by this Client
+     */
     fun getMoveResult(): CommandResult {
         return Server.getCommandResult(sessionId)
     }
